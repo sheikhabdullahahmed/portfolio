ddd2fc96737d14646701ffa5bb8479423ed2c29a
Your Name <sheikhabdullah456677@gmail.com>
2025-01-29 02:55:19 +0500

    not done yet

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,12 +5,9 @@ export default function Hero() {
   return (
     <div className='bg-black text-white text-center py-16 '>
         <img className='mx-auto mb-8 w-48  rounded-full object-cover transfrom  transition-transform duration-300  hover:scale-105' src="" alt="" />
-        <h1 className='text-4xl font-bold  '>
-            I,m {""}
-
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400  to-blue-500'>Sheikh Abdullah Ahmed</span>
-            Mern-Stack Developer
-        </h1>
+       <h1>
+        Mer Stack
+       </h1>
         <p className='mt-4 text-lg text-gray-300 '>I specialize in building modern and responsive web application </p>
         <div className='mt-8 space-x-4'>
             <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect with me </button>
